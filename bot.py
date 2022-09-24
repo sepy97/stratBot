4d04904078f78ecb1cfe4fd77bf8fff5e3ecf9b0
sepy97 <sepy97@gmail.com>
2022-09-24 09:14:02 -0700

    Added Candle class

--- bot.py
+++ bot.py
@@ -1,5 +1,20 @@
+import config as cfg
+import candles
+
+def print_kinds_of_candles(ticker):
+    kinds = []
+    with open(ticker+".csv", "r") as f:
+        lines = f.readlines()
+        for i in range(1, len(lines)):
+            inp_candle = lines[i].split(",")
+            prev_candle = lines[i-1].split(",")
+            candle = candles.Candle(inp_candle[0], inp_candle[1], inp_candle[2], inp_candle[3], inp_candle[4], prev_candle[2], prev_candle[3])
+            kinds.append(candle.to_string())
+    print(kinds)
+
 def main():
-    print("Hello World!")
-    
+    #cfg.export_daily_candles("AAPL")
+    print_kinds_of_candles("AAPL")
+
 if __name__ == '__main__':
     main()
---
+++ candles.py
@@ -0,0 +1,36 @@
+class Candle:
+    # Financial candle
+    def __init__(self, date, open, high, low, close, prev_high, prev_low):
+        self.date = date
+        self.open = open
+        self.high = high
+        self.low = low
+        self.close = close
+        self.previous_high = prev_high
+        self.previous_low = prev_low
+    
+    def __str__(self):
+        return "Date: " + self.date + " Open: " + self.open + " High: " + self.high + " Low: " + self.low + " Close: " + self.close 
+    
+    def get_kind(self):
+        # Return candle kind, either 1, 2 or 3
+        # 1 - inside candle
+        # 2 - directional candle
+        # 3 - outside candle
+        if self.high < self.previous_high and self.low > self.previous_low:
+            return "1"
+        elif self.high > self.previous_high and self.low < self.previous_low:
+            return "3"
+        elif (self.high > self.previous_high and self.low > self.previous_low) or (self.high < self.previous_high and self.low < self.previous_low):
+            return "2"
+    
+    def get_direction(self):
+        # Return candle direction
+        if self.open < self.close:
+            return "U"
+        else:
+            return "D"
+
+    def to_string(self):
+        # Return candle as string
+        return self.get_kind() + self.get_direction()
